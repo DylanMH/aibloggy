0820fa91d5a2dc98e5b07869645d5fceda777b39
DylanMH <53280664+DylanMH@users.noreply.github.com>
2023-08-21 18:06:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // To parse JSON in request bodies
 
-app.get("/generate", async (req, res) => {
+app.get("./backend", async (req, res) => {
   const { input } = req.query;
   const apiKey = process.env.OPEN_AI_KEY; // Access the API key from environment variable
 
