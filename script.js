263c25a3399430b76ae821399d2ed2efaf0321a2
DylanMH <53280664+DylanMH@users.noreply.github.com>
2023-08-23 17:45:41 -0500

    butt

--- script.js
+++ script.js
@@ -1,40 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document
-    .getElementById("submitUserInput")
-    .addEventListener("click", async () => {
-      const userInput = document.getElementById("userInput").value;
-
-      console.log("fetching content");
-      const response = await fetch(
-        `https://server.aibloggy.com:443/backend?input=${userInput}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-
-      console.log(response.status);
-      if (response.ok) {
-        const generatedContent = await response.text();
-        if (generatedContent) {
-          document.getElementById("blogContent").innerHTML =
-            formatContent(generatedContent);
-        }
-      } else {
-        console.log(response.statusText);
-      }
-    });
-});
-
-// Rest of your script.js code
-
-const storedContent = localStorage.getItem("generatedContent");
-if (storedContent) {
-  document.getElementById("blogContent").innerHTML = storedContent;
-}
-
 const formatContent = (content) => {
   const paragraphs = content.split("\n\n");
   const title = paragraphs.shift();
@@ -46,3 +9,61 @@ const formatContent = (content) => {
 
   return `${formattedTitle}${formattedParagraphs}`;
 };
+
+let isButtonEnabled = false;
+const BUTTON_DELAY = 5000;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const submitButton = document.getElementById("submitUserInput");
+
+  submitButton.addEventListener("click", async () => {
+    if (!isButtonEnabled) {
+      isButtonEnabled = true; // disable the blog button
+    }
+
+    document.getElementById("blogContent").innerHTML = ""; // clear the blog content
+    const loadingSpinner = document.getElementById("loadingSpinner");
+    loadingSpinner.style.display = "inline-block"; // show the spinner
+    submitButton.disabled = true; // disable the submit button
+
+    const userInput = document.getElementById("userInput").value;
+
+    console.log("fetching content");
+    try {
+      const response = await fetch(
+        `https://server.aibloggy.com:443/backend?input=${userInput}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const generatedContent = await response.text();
+        if (generatedContent) {
+          document.getElementById("blogContent").innerHTML =
+            formatContent(generatedContent);
+          localStorage.setItem("generatedContent", generatedContent);
+        }
+      } else {
+        console.log(response.statusText);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      loadingSpinner.style.display = "none"; // Hide the spinner
+      setTimeout(() => {
+        isButtonEnabled = false; // reenable the blog button
+        submitButton.disabled = false; // reenable the submit button
+      }, BUTTON_DELAY);
+    }
+  });
+});
+
+const storedContent = localStorage.getItem("generatedContent");
+if (storedContent) {
+  document.getElementById("blogContent").innerHTML =
+    formatContent(storedContent);
+}
