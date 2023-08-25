1bae47f741f42870ccdc34848c7fb8bb3f43c101
DylanMH <53280664+DylanMH@users.noreply.github.com>
2023-08-25 01:28:14 -0500

    V1.1 Bloggy is Born
    Upated Styles and added read more content button

--- script.js
+++ script.js
@@ -1,37 +1,65 @@
+// global variables
+let isButtonEnabled = false;
+const BUTTON_DELAY = 5000;
+
+// format generated content function
 const formatContent = (content) => {
   const paragraphs = content.split("\n\n");
   const title = paragraphs.shift();
 
   const formattedTitle = `<h1 class="text-center">${title}</h1>`;
-  const formattedParagraphs = paragraphs
+  // remove any conclusion paragraph this stupid ai might generate
+  const paragraphsWithoutLast = paragraphs.slice(0, -1);
+
+  const formattedParagraphs = paragraphsWithoutLast
     .map((paragraph) => `<p class="text-indent">${paragraph}</p>`)
     .join("");
 
   return `${formattedTitle}${formattedParagraphs}`;
 };
 
-let isButtonEnabled = false;
-const BUTTON_DELAY = 5000;
+// logic to stop blogContent from clearing on refresh
+const storedContent = localStorage.getItem("generatedContent");
+if (storedContent) {
+  document.getElementById("blogContent").innerHTML =
+    formatContent(storedContent);
+}
 
+// main initialization function
 document.addEventListener("DOMContentLoaded", () => {
   const submitButton = document.getElementById("submitUserInput");
+  const moreContentButton = document.getElementById("moreContent");
 
+  const isContentLoaded =
+    storedContent || document.getElementById("blogContent");
+  if (isContentLoaded > 0) {
+    moreContentButton.disabled = false;
+  } else {
+    moreContentButton.disabled = true;
+  }
+
+  // Event listener for the submit button
   submitButton.addEventListener("click", async () => {
     if (!isButtonEnabled) {
       isButtonEnabled = true; // disable the blog button
     }
 
     document.getElementById("blogContent").innerHTML = ""; // clear the blog content
-    const loadingSpinner = document.getElementById("loadingSpinner");
-    loadingSpinner.style.display = "inline-block"; // show the spinner
+
+    // display a loading spinner on click
+    const generateContentSpinner = document.getElementById(
+      "generateContentSpinner"
+    );
+    generateContentSpinner.style.display = "inline-block"; // show the spinner
     submitButton.disabled = true; // disable the submit button
 
     const userInput = document.getElementById("userInput").value;
+    const aiInput = `generate a 3 paragraph blog without a conclusion and return it with no labels or prefixes but keep the title. The topic to write about is "${userInput}". Do not include "In conclusion".`;
 
     console.log("fetching content");
     try {
       const response = await fetch(
-        `https://server.aibloggy.com:443/backend?input=${userInput}`,
+        `https://server.aibloggy.com:443/backend?input=${aiInput}`,
         {
           method: "GET",
           headers: {
@@ -53,17 +81,57 @@ document.addEventListener("DOMContentLoaded", () => {
     } catch (error) {
       console.error(error);
     } finally {
-      loadingSpinner.style.display = "none"; // Hide the spinner
+      generateContentSpinner.style.display = "none"; // Hide the spinner
+      moreContentButton.disabled = false; // enable the more content button
       setTimeout(() => {
         isButtonEnabled = false; // reenable the blog button
         submitButton.disabled = false; // reenable the submit button
       }, BUTTON_DELAY);
     }
   });
-});
 
-const storedContent = localStorage.getItem("generatedContent");
-if (storedContent) {
-  document.getElementById("blogContent").innerHTML =
-    formatContent(storedContent);
-}
+  // Event listener for more content button
+  moreContentButton.addEventListener("click", async () => {
+    moreContentButton.disabled = true; // disable the more content button
+
+    const currentContent =
+      document.getElementById("blogContent").innerHTML ||
+      localStorage.getItem("generatedContent"); // take out any html syntax for the ai interpreter
+
+    // display a loading spinner on click
+    const moreContentSpinner = document.getElementById("moreContentSpinner");
+    moreContentSpinner.style.display = "inline-block"; // show the spinner
+
+    // prompt that is sent to ai engine to generate the content
+    const aiInput = `use the following content to generate 3 additional paragraphs and add them to the end original content. Return the whole prompt new content without any labels or prefixes, but keep the title. The content: "${currentContent}". Do not include "In conclusion"`;
+
+    console.log("fetching content");
+    try {
+      const response = await fetch(
+        `https://server.aibloggy.com:443/backend?input=${aiInput}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const generatedContent = await response.text();
+        // if content is generated take insert it into the blog content element with the already generated blog content
+        if (generatedContent) {
+          document.getElementById("blogContent").innerHTML =
+            formatContent(generatedContent);
+          localStorage.setItem("generatedContent", generatedContent);
+        }
+      } else {
+        console.log(response.statusText);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      moreContentSpinner.style.display = "none"; // Hide the spinner
+    }
+  });
+});
